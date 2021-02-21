dbe7f7aa1d32f580196a9a0137378f64870bbb09
James Carman <jcarman@callibrity.com>
2021-02-20 20:17:21 -0500

    Initial version.

---
+++ src/main/java/com/callibrity/kubert/KubertApplication.java
@@ -0,0 +1,13 @@
+package com.callibrity.kubert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callibrity/kubert/KubertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callibrity.kubert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
